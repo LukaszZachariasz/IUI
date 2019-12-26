595b7196109a7c857002da61c7ee7088a56aaf0f
LukaszZachariasz <lukaszzachariasz.contactme@gmail.com>
2019-12-26 19:48:26 +0100

    backend init

---
+++ back-end/food-order-back/src/main/java/com/foodorderback/foodorderback/FoodOrderBackApplication.java
@@ -0,0 +1,13 @@
+package com.foodorderback.foodorderback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderBackApplication.class, args);
+	}
+
+}
