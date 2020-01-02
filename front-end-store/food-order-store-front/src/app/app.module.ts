136acf103044d252501f5d883dd43f22e7ab1cc6
LukaszZachariasz <lukaszzachariasz.contactme@gmail.com>
2020-01-02 14:30:20 +0100

    Added Navbar and MdBootstrap Library

--- front-end-store/food-order-store-front/src/app/app.module.ts
+++ front-end-store/food-order-store-front/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import 'hammerjs';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
